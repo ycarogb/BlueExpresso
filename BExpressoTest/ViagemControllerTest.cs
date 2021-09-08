7125cb2adda70d637f8c371b9c82e0c8c3a34c42
Ycaro <ycarobatalha1@gmail.com>
2021-09-08 18:07:15 -0300

    Incuindo Testes e ReadMe

---
+++ BExpressoTest/ViagemControllerTest.cs
@@ -0,0 +1,83 @@
+﻿using BlueExpresso.API;
+using BlueExpresso.Controllers;
+using BlueExpresso.Models;
+using BlueExpresso.Services;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BExpressoTest
+{
+    public class ViagemControllerTest
+    {
+        int viagemQuantity = 10;
+        List<Viagem> fakeViagens;
+
+        public ViagemControllerTest()
+        {
+            fakeViagens = new List<Viagem>();
+            for (var i = 1; i < viagemQuantity; i++)
+                fakeViagens.Add(new Viagem { Id = i, Cliente = $"Cliente {i}" });
+        }
+
+        [Fact]
+        public void GetViagens_Returns_The_Correct_Viagens()
+        {
+            var productService = A.Fake<IViagemService>();
+            A.CallTo(() => productService.GetAll()).Returns(fakeViagens); //Quando chamar o All, retorne fake Products
+            var controller = new ViagemController(productService);
+
+            OkObjectResult result = controller.Index() as OkObjectResult;
+
+            var values = result.Value as APIResponse<List<Viagem>>;
+
+            Assert.True(
+                values.Results == fakeViagens &&
+                values.Message == "" &&
+                values.Succeed
+                );
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(8)]
+        [InlineData(13, "Viagem não encontrada!", false)]
+        [InlineData(0, "Viagem não encontrada!", false)]
+        [InlineData(-98, "Viagem não encontrada!", false)]
+        public void GetViagem_Returns_Viagem_By_Id(int id, string message = "", bool succeed = true)
+        {
+            var productService = A.Fake<IViagemService>();
+            A.CallTo(() => productService.Get(id)).Returns(fakeViagens.Find(p => p.Id == id));
+
+            var controller = new ViagemController(productService);
+
+            ObjectResult result = controller.Index(id) as ObjectResult;
+
+            var exists = fakeViagens.Find(p => p.Id == id) != null;
+
+            if (exists)
+            {
+                var values = result.Value as APIResponse<Viagem>;
+                Assert.True(
+                    values.Succeed == succeed &&
+                    values.Message == message &&
+                    values.Results == fakeViagens.Find(p => p.Id == id)
+                    );
+            }
+            else
+            {
+                var values = result.Value as APIResponse<string>;
+                Assert.True(
+                    values.Succeed == succeed &&
+                    values.Message == message &&
+                    values.Results == null
+                    );
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+
+# BlueExpresso
+
+## Descrição do Projeto
+
+API para gerenciamento de tickets de viagem de uma empresa fictícia do ramo do transporte rodoviário, intitulada BlueExpresso
+
+Este projeto foi desenvolvido durante o módulo 4 do Curso de Programação na BLUE EdTech
+
+[![Generic badge](https://img.shields.io/badge/Version-1.0-<COLOR>.svg)](https://shields.io/)
+
+## Estrutura da Aplicação
+
+Antes de utilizar a API pela primeira vez, o usuário deve cadastrar um usuário de senha para obter um token de autorização. Siga esses passos:
+
+	* no endpoint "~/Auth/Register" digite, em linguagem json, o login e senha desejados. 
+
+	* a seguir, no endpoint "~/Auth/Token" você deve inserir o login e senha cadastrados para obter o Token de autorização
+
+	* Para acessar os endpoints da API, o usuário deve inserir o Token na opção "Bearer Authentication"
+
+Atenção!!
+O token tem prazo de 2 horas para expirar.
+
+## Tecnologias Utilizadas
+Esse projeto foi desenvolvido no framework .NET Core 5.0
+
+## Autor
+ <img style="border-radius: 80%;" src="https://i1.sndcdn.com/avatars-001002863491-80v8qp-t500x500.jpg" width="100px;" alt=""/>
+<br />
+Feito com ❤️ por Ycaro Batalha
+
+<br />
+👋🏽 Entre em contato!
+<br />
+
+[![Linkedin Badge](https://img.shields.io/badge/-Ycaro-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/ycaro-gabriel-da-costa-batalha-2019/)](https://www.linkedin.com/in/ycaro-gabriel-da-costa-batalha-2019/) 
